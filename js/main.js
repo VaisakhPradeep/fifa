b3da69f98d364192cf744e10c8f3550e2ef38683
VaisakhPradeep <92vaisakh92@gmail.com>
2018-06-29 00:22:33 +0530

    Revert to e1a307

--- js/main.js
+++ js/main.js
@@ -648,17 +648,17 @@ $(".match44").click(function(){
 	window.open("https://youtu.be/_dusyi-4pMs");
 });
 
+/*
+
 $(".match45").addClass("score");
 $(".match45").click(function(){
-	window.open("https://www.youtube.com/watch?v=2CRmHuN-O84");
+	window.open("https://youtu.be/AygUlfmQgBs");
 });
 
 $(".match46").addClass("score");
 $(".match46").click(function(){
-	window.open("https://www.youtube.com/watch?v=K7pVlD8Q660");
+	window.open("https://youtu.be/AygUlfmQgBs");
 });
-
-/*
 
 $(".match47").addClass("score");
 $(".match47").click(function(){
